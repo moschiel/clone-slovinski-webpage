ee48d616385fe4c05807af5977f744be38bd1dd1
M@schie1 <moschiel@hotmail.com>
2020-10-13 03:45:42 -0300

    updated vanillaJs to jQuery, added Slick plugin

--- js/script.js
+++ js/script.js
@@ -26,94 +26,51 @@ $('nav a').click(function(e){
   //return false; //pra nao redirecionar a pagina por ter clica em um link
 });
 
-
+//evento para trocar tratamento ao clicar nos seletores
 const cor_ativo = "#688293";
 const cor_inativo = "#d8d8d8";
 
-const trocar_tratamento = (seletor) => {
+var seletores = $('.tratamentos-selector .selector');
+//const trocar_tratamento = (seletor) => {
+seletores.click( (e) => { 
+  let tratamentos = $('.column-tratamentos');
+  let seletorIndex = seletores.index($(e.target));
 
-  column1 = document.getElementById("col1");
-  column2 = document.getElementById("col2");
-  column3 = document.getElementById("col3");
+  seletores.css('background-color', cor_inativo);
+  $(e.target).css('background-color', cor_ativo);
 
-  seletor1 = document.getElementById("seletor1");
-  seletor2 = document.getElementById("seletor2");
-  seletor3 = document.getElementById("seletor3");
-
-  seletor1.style.backgroundColor = cor_inativo;
-  seletor2.style.backgroundColor = cor_inativo;
-  seletor3.style.backgroundColor = cor_inativo;
-  
-  //Se width maior que 480px, apenas dois seletores
-  if(screen.width > 480) 
-  {
-    if(seletor == "seletor1")
-    {
-      column1.style.display = "block";
-      column3.style.display = "none";
-
-      seletor1.style.backgroundColor = cor_ativo;
+  //Se width maior que 480px, apenas dois seletores (dois tratamentos na tela)
+  if(screen.width > 480) {
+    tratamentos.css('display', 'block');
+    if(seletorIndex == 0) {
+      tratamentos.eq(2).css('display', 'none');
     }
-    else if(seletor == "seletor2")
-    {
-      column1.style.display = "none";
-      column3.style.display = "block";
-
-      seletor2.style.backgroundColor = cor_ativo;
+    else if(seletorIndex == 1) {
+      tratamentos.eq(0).css('display', 'none');
     }
   }
-  else //Senao, se width <= 480px, tres seletores
-  {
-    if(seletor == "seletor1")
-    {
-      column1.style.display = "block";
-      column2.style.display = "none";
-      column3.style.display = "none";
-
-      seletor1.style.backgroundColor = cor_ativo;
-    }
-    else if(seletor == "seletor2")
-    {
-      column1.style.display = "none";
-      column2.style.display = "block";
-      column3.style.display = "none";
-
-      seletor2.style.backgroundColor = cor_ativo;
-    }
-    else if(seletor == "seletor3")
-    {
-      column1.style.display = "none";
-      column2.style.display = "none";
-      column3.style.display = "block";
-
-      seletor3.style.backgroundColor = cor_ativo;
-    }
+  else { //Senao, se width <= 480px, tres seletores (um unico tratamento na tela)
+    tratamentos.css('display', 'none');
+    tratamentos.eq(seletorIndex).css('display', 'block');
   }
-}
+});
+
+//evento para trocar depoimento ao clicar nos seletores
+var seletoresDepo = $('.depo-selector .selector');
+seletoresDepo.click((e) => {
+  let depoimentos = $('.depo');
+  let seletorIndex = seletoresDepo.index($(e.target));
+
+  seletoresDepo.css('background-color', cor_inativo);
+  $(e.target).css('background-color', cor_ativo);
+
+  depoimentos.css('display', 'none');
+  depoimentos.eq(seletorIndex).css('display', 'flex');
+});
 
 
 
-
-function trocar_depo(seletor){
-  console.log(seletor);
-   depo1 = document.getElementById('depo1');
-   depo2 = document.getElementById('depo2');
-  
-
-  let select1 = document.getElementById('select1'); 
-  let select2 = document.getElementById('select2');
-
-  if(seletor == 'select1'){
-    depo2.style.display = "none";
-    depo1.style.display = "flex";
-    select1.style.backgroundColor = cor_ativo;
-    select2.style.backgroundColor =  cor_inativo
-  }
-  
-  if(seletor == 'select2'){
-    depo1.style.display = "none"; 
-    depo2.style.display = "flex";
-    select2.style.backgroundColor = cor_ativo;
-    select1.style.backgroundColor = cor_inativo;
-  }
-}
+/************* SLICK jQuery ******************* */
+$(function() {
+  //$('.mosaico1').slick();
+})
